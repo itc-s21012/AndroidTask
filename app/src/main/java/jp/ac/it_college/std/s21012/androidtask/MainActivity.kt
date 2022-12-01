6461a3ef547b5c4d8ce38f523ce985f9c685d84d
itc-s21012 <s21012@std.it-college.ac.jp>
2022-12-01 10:38:31 +0900

    フラグメント追加

--- app/src/main/java/jp/ac/it_college/std/s21012/androidtask/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21012/androidtask/MainActivity.kt
@@ -2,10 +2,28 @@ package jp.ac.it_college.std.s21012.androidtask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import jp.ac.it_college.std.s21012.androidtask.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    //    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+//    private var pokemonList: List<Poke>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initPokemonList()
+
+
+        //画面遷移をわかりやすくするため
+        val navController =
+            binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        binding.toolbar.setupWithNavController(
+            navController,
+            AppBarConfiguration(navController.graph)
+        )
     }
-}
